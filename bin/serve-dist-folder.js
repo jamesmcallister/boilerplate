3e1f7fb67e1015dd2ed4d44c72d70b99e8903f89
James McAllister <james@mcallister.xyz>
2016-07-27 19:32:03 +0100

    linting

--- bin/serve-dist-folder.js
+++ bin/serve-dist-folder.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+var express = require('express')
+var path = require('path')
+var app = express()
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './../dist/index.html'));});
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './../dist/index.html'))
+})
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  console.log('Example app listening on port 3000!')
+})
